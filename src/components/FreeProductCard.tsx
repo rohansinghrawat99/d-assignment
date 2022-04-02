da3fd046d48cf94f25e5debcb28f992144af6f5b
Rohan Singh Rawat <rsrofficial99@gmail.com>
2022-04-02 18:36:40 +0530

    [REMOVED]Removed scrollbar

--- src/components/FreeProductCard.tsx
+++ src/components/FreeProductCard.tsx
@@ -10,7 +10,7 @@ const FreeProductCard: React.FC<FreeProductCardProps> = ({title, description, im
         <div className='flex flex-col w-88'>
             <img src={image_url} alt="" className='mb-4'/>
             <h1 className='text-sm md:text-lg font-galanoGrotesqueSemiBold font-semibold mb-1'>{title}</h1>
-            <p className='text-xs md:text-[16px] text-smallText tracking-wider'>{description}</p>
+            <p className='text-xs md:text-[16px] text-smallText tracking-widest'>{description}</p>
         </div>
     )
 }
